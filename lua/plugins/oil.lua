0545d8a5342ea14eeaa7632375c94b3dec21cbfb
soemre <emresoysuren03@gmail.com>
2024-12-04 17:26:19 +0100

    configured oil

--- lua/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"stevearc/oil.nvim",
-	enabled = false, -- not right now
-	-- TODO:Configure
-	---@module 'oil'
-	---@type oil.SetupOpts
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-	opts = {},
-}
