d9affa93e26acfd58937c3208b1a15d038ea5449
soemre <emresoysuren03@gmail.com>
2024-12-07 00:20:42 +0100

    fix: rust linter | update: do not start copilot on start

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,9 @@
 return {
 	"github/copilot.vim",
-	event = "InsertEnter",
+	cmd = "Copilot",
 	config = function()
+		vim.cmd("Copilot disable")
+
 		vim.keymap.set("i", "<C-a-o>", 'copilot#Accept("\\<CR>")', {
 			expr = true,
 			replace_keycodes = false,
